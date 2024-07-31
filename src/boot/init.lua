208bc98b6fb7bda2a052dd0cf12fcd4521dee80f
Kevin Alavik <kevin@alavik.se>
2024-07-31 09:47:27 +0200

    feat: Made splash screen fade in as well

--- src/boot/init.lua
+++ src/boot/init.lua
@@ -1,5 +1,6 @@
 function initWindow()
-    love.window.setTitle("Random Ass Game (RAG)")
+    love.window.setTitle("Random Ass Game (RAG)");
+    love.graphics.setBackgroundColor(0.1, 0.1, 0.1, 1);
 end
 
-return { initWindow = initWindow }
+return { initWindow = initWindow };
--- src/boot/splash.lua
+++ src/boot/splash.lua
@@ -1,7 +1,8 @@
 local splash_screen = {}
-local alpha = 1
+local alpha = 0
 local fade_speed = 0.5
 local logo, width, height
+local state = "fadein"
 local done = false
 
 function splash_screen.load(logo_path, optional_fade_speed)
@@ -12,14 +13,23 @@ function splash_screen.load(logo_path, optional_fade_speed)
         fade_speed = optional_fade_speed
     end
 
-    done = false;
-    alpha = 1;
+    done = false
+    alpha = 0
+    state = "fadein"
 end
 
 function splash_screen.update(dt)
-    if alpha > 0 then
+    if state == "fadein" then
+        alpha = alpha + (fade_speed * 1.5) * dt
+        if alpha >= 1 then
+            alpha = 1
+            state = "hold"
+        end
+    elseif state == "hold" then
+        state = "fadeout"
+    elseif state == "fadeout" then
         alpha = alpha - fade_speed * dt
-        if alpha < 0 then
+        if alpha <= 0 then
             alpha = 0
             done = true
         end
